12ae8b99541ab1941deafe44d8c041d5b6ccee26
Aboussafi-oumaima <101676250+Aboussafi-oumaima@users.noreply.github.com>
2025-06-02 18:04:32 +0100

    Update socket.jsx

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,6 +1,6 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const socket = io(`${process.env.REACT_APP_API_URL}`); // adjust your server URL/port
+const socket = io("https://projet-recrutement.onrender.com");
 
 export default socket;
