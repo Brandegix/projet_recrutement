55760d5177e9d7bb4efd9308510ec2f58d5bd65c
Aboussafi-oumaima <101676250+Aboussafi-oumaima@users.noreply.github.com>
2025-06-19 13:30:17 +0100

    Update SavedJobOffers.jsx

--- frontend/src/components/Candidat/SavedJobOffers.jsx
+++ frontend/src/components/Candidat/SavedJobOffers.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { FaMapMarkerAlt, FaBriefcase, FaBookmark, FaRegBookmark, 
 FaClock, FaHeart, FaRegHeart, FaSearch } from 'react-icons/fa';
 import "../../assets/css/JobCards.css"; // Ensure this CSS file is used 
-for JobCard specific styles
+
 import Footer from '../../components/Footer';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
