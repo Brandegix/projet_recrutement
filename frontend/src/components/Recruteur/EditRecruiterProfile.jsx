a6fab289554cf7b86962e20e941e026b2a963576
Aboussafi-oumaima <101676250+Aboussafi-oumaima@users.noreply.github.com>
2025-06-24 09:28:22 +0100

    Update EditRecruiterProfile.jsx

--- frontend/src/components/Recruteur/EditRecruiterProfile.jsx
+++ frontend/src/components/Recruteur/EditRecruiterProfile.jsx
@@ -427,7 +427,7 @@ function EditRecruiterProfile() {
                   <button
                     type="button"
                     onClick={() => navigate("/RecruiterProfile")}
-                    style={styles.cancelButtons}
+                    style={styles.submitButtons}
                     className="cancel-buttons"
                   >
                     Annuler
@@ -449,35 +449,7 @@ function EditRecruiterProfile() {
                       </>
                     ) : (
                       <>
-                        <svg
-                          width="20"
-                          height="20"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M19 21H5C4.44772 21 4 20.5523 4 20V4C4 3.44772 4.44772 3 5 3H16L20 7V20C20 20.5523 19.5523 21 19 21Z"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                          <path
-                            d="M17 21V13H7V21"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                          <path
-                            d="M7 3V8H15"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
+                      
                         Enregistrer
                       </>
                     )}
