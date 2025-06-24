aeb8c93ae77b35c5fa46bac62e28ac5f0a8125ce
Aboussafi-oumaima <101676250+Aboussafi-oumaima@users.noreply.github.com>
2025-06-24 16:22:42 +0100

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/Footer.css';
-import logo from '../assets/ourlogo1.jpeg'; // adjust the path if needed
+import logo from '../assets/ourlogo.png'; // adjust the path if needed
 
 
 const Footer = () => {
